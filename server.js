5cc4184116b09bde1cd0610abeecdd24fbbfa9a1
omarrmdn <omarrmdn2024@gmail.com>
2025-12-12 01:50:31 +0200

    Ignore environment files

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-import express from "express"
-
-import dotenv from "dotenv"
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
 
 const app = express();
 
 dotenv.config({
-    path: "./config/config.env"
+  path: "./config/config.env",
 });
+
+app.use(morgan("dev"))
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("omar")
-})
+app.get("/", (req, res) => {
+  res.send("omar");
+});
 
-app.listen(3000,
-    console.log("server on 3000")
-)
-
+app.listen(3000, console.log("server on 3000"));
